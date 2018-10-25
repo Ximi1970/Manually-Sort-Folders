076a43d6c4c0b8e30532d64bd7e474b4fd1f2e87
Maxime Rijnders <ximi.github@gmail.com>
2018-10-25 13:40:02 +0200

    Add bootstrap base

---
+++ bootstrap.js
@@ -0,0 +1,110 @@
+// https://flagfox.wordpress.com/2014/01/19/writing-restartless-addons/
+
+
+
+Components.utils.import("resource://gre/modules/Services.jsm");
+
+
+const PREF_BRANCH = "extensions.tbsortfolders@xulforum.org.";
+const PREFS = {
+  tbsf_data: '{}',
+  startup_folder: "",
+};
+
+function setDefaultPrefs() {
+  let branch = Services.prefs.getDefaultBranch(PREF_BRANCH);
+  for (let key in PREFS) {
+    switch (typeof PREFS[key]) {
+      case "boolean":
+        branch.setBoolPref(key, PREFS[key]);
+        break;
+      case "number":
+        branch.setIntPref(key, PREFS[key]);
+        break;
+      case "string":
+        branch.setCharPref(key, PREFS[key]);
+        break;
+      case "object":
+        branch.setObjPref(key, PREFS[key]);
+        break;
+    }
+  }
+}
+
+function getPref(key) {
+  // Cache the prefbranch after first use
+  if (getPref.branch == null)
+    getPref.branch = Services.prefs.getBranch(PREF_BRANCH);
+  // Figure out what type of pref to fetch
+  switch (typeof PREFS[key]) {
+    case "boolean":
+      return getPref.branch.getBoolPref(key);
+    case "number":
+      return getPref.branch.getIntPref(key);
+    case "string":
+      return getPref.branch.getCharPref(key);
+    case "object":
+      return getPref.branch.getObjectPref(key);
+  }
+}
+
+function startup(data,reason) {
+    
+    setDefaultPrefs();
+    
+//    myModule.startup();  // Do whatever initial startup stuff you need to do
+
+    forEachOpenWindow(loadIntoWindow);
+    Services.wm.addListener(WindowListener);
+}
+
+function shutdown(data,reason) {
+    if (reason == APP_SHUTDOWN)
+        return;
+
+    forEachOpenWindow(unloadFromWindow);
+    Services.wm.removeListener(WindowListener);
+
+//    myModule.shutdown();  // Do whatever shutdown stuff you need to do on addon disable
+
+    // HACK WARNING: The Addon Manager does not properly clear all addon related caches on update;
+    //               in order to fully update images and locales, their caches need clearing here
+    Services.obs.notifyObservers(null, "chrome-flush-caches", null);
+}
+
+function install(data,reason) { }
+
+function uninstall(data,reason) { }
+
+function loadIntoWindow(window) {
+/* call/move your UI construction function here */
+}
+
+function unloadFromWindow(window) {
+/* call/move your UI tear down function here */
+}
+
+function forEachOpenWindow(todo)  // Apply a function to all open browser windows
+{
+    var windows = Services.wm.getEnumerator("navigator:browser");
+    while (windows.hasMoreElements())
+        todo(windows.getNext().QueryInterface(Components.interfaces.nsIDOMWindow));
+}
+
+var WindowListener =
+{
+    onOpenWindow: function(xulWindow)
+    {
+        var window = xulWindow.QueryInterface(Components.interfaces.nsIInterfaceRequestor)
+                              .getInterface(Components.interfaces.nsIDOMWindow);
+        function onWindowLoad()
+        {
+            window.removeEventListener("load",onWindowLoad);
+            if (window.document.documentElement.getAttribute("windowtype") == "navigator:browser")
+                loadIntoWindow(window);
+        }
+        window.addEventListener("load",onWindowLoad);
+    },
+    onCloseWindow: function(xulWindow) { },
+    onWindowTitleChange: function(xulWindow, newTitle) { }
+};
